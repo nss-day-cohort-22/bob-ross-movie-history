38c13ba14bf70cb63ee8ec7a4d62230d19caef62
Kristen Norris <krnorris65@gmail.com>
2018-02-26 12:00:34 -0600

    user can make an api call to search movies

--- MovieHistory/MovieHistory/wwwroot/js/site.js
+++ MovieHistory/MovieHistory/wwwroot/js/site.js
@@ -1,5 +1,19 @@
 ﻿// Write your JavaScript code.
 
 $("#movieSearch__button").click(evt => {
+    //what the user searches for
     const userSearchString = $("#movieSearch").val()
+    //function to make the api call with user's search
+    function searchMovieAPI(userSearch) {
+        //if the userSearch is more than one word, split the string at the space and join back together with a plus sign (capitalization does not matter for the api search)
+        let apiSearch = userSearch.split(" ").join("+")
+        $.ajax({
+            "url": `https://api.themoviedb.org/3/search/movie?api_key=858deec9a8305f575390bb92f4c3eab8&query=${apiSearch}`,
+               "method": "GET"
+        }).then(apiResults => {
+            console.log(apiResults)
+        })
+    }
+    //passing the user's search into the function to make the api call
+    searchMovieAPI(userSearchString)
 });
