5212565136df69998351fd3c9ca9307af79daeb1
Kristen Norris <krnorris65@gmail.com>
2018-02-26 13:19:42 -0600

    api call added to site.js

--- MovieHistory/MovieHistory/wwwroot/js/site.js
+++ MovieHistory/MovieHistory/wwwroot/js/site.js
@@ -3,17 +3,12 @@
 $("#movieSearch__button").click(evt => {
     //what the user searches for
     const userSearchString = $("#movieSearch").val()
-    //function to make the api call with user's search
-    function searchMovieAPI(userSearch) {
-        //if the userSearch is more than one word, split the string at the space and join back together with a plus sign (capitalization does not matter for the api search)
-        let apiSearch = userSearch.split(" ").join("+")
-        $.ajax({
-            "url": `https://api.themoviedb.org/3/search/movie?api_key=858deec9a8305f575390bb92f4c3eab8&query=${apiSearch}`,
-               "method": "GET"
-        }).then(apiResults => {
-            console.log(apiResults)
-        })
-    }
-    //passing the user's search into the function to make the api call
-    searchMovieAPI(userSearchString)
+       //ajax call using the user search
+    $.ajax({
+        "url": `https://api.themoviedb.org/3/search/movie?api_key=858deec9a8305f575390bb92f4c3eab8&query=${userSearchString}`,
+        "method": "GET"
+    }).then(apiResults => {
+        console.log(apiResults)
+    })
+
 });
