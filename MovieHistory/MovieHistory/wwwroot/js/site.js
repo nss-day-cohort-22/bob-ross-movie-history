b1d4a44aff094b715e71687754903f86691d69ab
Steve Brownlee <chortlehoort@gmail.com>
2018-02-26 15:54:54 -0600

    Showing all movies in a grid

--- MovieHistory/MovieHistory/wwwroot/js/site.js
+++ MovieHistory/MovieHistory/wwwroot/js/site.js
@@ -4,13 +4,21 @@
         method: "GET",
         url: `https://api.themoviedb.org/3/search/movie?api_key=6cc5adb954efa8ba18b1ed8d19b4b1bf&language=en-US&query=${userSearchString}&page=1&include_adult=false`
     }).then(res => {
-        let titles = ""
-        res.results.forEach(m => {
+        let titles = "<div class='row'>"
+        res.results.forEach((m, idx) => {
+            
             titles += `
-                <h2>${m.title}</h2>
-                <img src="https://image.tmdb.org/t/p/w154${m.poster_path}" />
+                <div class="col-md-3 movieGrid__movie">
+                    <h2>${m.title}</h2>
+                    <img src="https://image.tmdb.org/t/p/w154${m.poster_path}" />
+                </div>
             `
+            if ((idx + 1) % 4 === 0) {
+                titles += "</div><div class='row'>"
+            }
         })
+
+        titles += "</div>"
 
         $("#movieGrid").html(titles)
     })
