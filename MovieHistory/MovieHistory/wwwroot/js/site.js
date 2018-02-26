9d7a5442892b3cfaaa56dbc689ce92656252b7aa
Garrett Ward <gww8804@gmail.com>
2018-02-26 13:21:27 -0800

    stashin and bashin

--- MovieHistory/MovieHistory/wwwroot/js/site.js
+++ MovieHistory/MovieHistory/wwwroot/js/site.js
@@ -2,4 +2,16 @@
 
 $("#movieSearch__button").click(evt => {
     const userSearchString = $("#movieSearch").val()
+
+    let keywords = userSearchString.replace(/ {2}/g, "+").replace(/ /g, "+")
+
+    let movieString = "http://www.omdbapi.com/?apikey=7bdab718&s=" + keywords
+
+    $.ajax({
+        "url": movieString,
+        "method": "GET"
+    }).then(result => {
+        console.log(result)
+    })
+
 });
