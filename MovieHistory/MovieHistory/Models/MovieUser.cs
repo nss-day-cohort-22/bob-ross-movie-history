a8e5d1d421cce23356eaffe05753b40d5e4eb9d6
Hannah Hall <hannahkhall13@gmail.com>
2018-02-26 09:34:47 -0600

    finished models

--- MovieHistory/MovieHistory/Models/MovieUser.cs
+++ MovieHistory/MovieHistory/Models/MovieUser.cs
@@ -1,10 +1,30 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace MovieHistory.Models
 {
     public class MovieUser
     {
         [Key]
+        // Field being indexed in db context
         public int MovieUserId { get; set; }
+
+        [Required]
+        public ApplicationUser User { get; set; }
+
+        [Required]
+        public int MovieId { get; set; }
+        public Movie Movie { get; set; }
+
+        public string Genre { get; set; }
+
+        public bool Favorited { get; set; }
+        public bool Watched { get; set; }
+
+        public MovieUser() {
+          Favorited = false;
+          Watched = false;
+        }
+        public virtual ICollection<Recommendation> Recommendations { get; set; }
     }
 }
--- MovieHistory/MovieHistory/Models/Recommendation.cs
+++ MovieHistory/MovieHistory/Models/Recommendation.cs
@@ -6,5 +6,12 @@ namespace MovieHistory.Models
     {
         [Key]
         public int RecommendationId { get; set; }
+        
+        [Required]
+        public ApplicationUser ToUser { get; set; }
+
+        [Required]
+        public int MovieUserId { get; set; }
+        public MovieUser MovieUser { get; set; }
     }
 }
