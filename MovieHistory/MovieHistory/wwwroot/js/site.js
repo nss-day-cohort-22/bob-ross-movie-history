e823c029418caf69f85a7d1b87b165dc7ef97424
Steve Brownlee <chortlehoort@gmail.com>
2018-02-26 15:20:17 -0600

    Attempt at api key configuration and showing titles/poster for searched movies

--- MovieHistory/MovieHistory/wwwroot/js/site.js
+++ MovieHistory/MovieHistory/wwwroot/js/site.js
@@ -2,7 +2,16 @@
     const userSearchString = $("#movieSearch").val()
     $.ajax({
         method: "GET",
-        url: `https://api.themoviedb.org/3/search/movie?api_key=ec2f1e210a4d2150e069b9b93b4a4c12&language=en-US&query=${userSearchString}&page=1&include_adult=false`,
-        success: (Response => console.log(Response))
+        url: `https://api.themoviedb.org/3/search/movie?api_key=6cc5adb954efa8ba18b1ed8d19b4b1bf&language=en-US&query=${userSearchString}&page=1&include_adult=false`
+    }).then(res => {
+        let titles = ""
+        res.results.forEach(m => {
+            titles += `
+                <h2>${m.title}</h2>
+                <img src="https://image.tmdb.org/t/p/w154${m.poster_path}" />
+            `
+        })
+
+        $("#movieGrid").html(titles)
     })
 });
