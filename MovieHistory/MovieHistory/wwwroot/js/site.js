26205c7abaa0bb70d2a2a6426c425f09c237e330
Kristen Norris <krnorris65@gmail.com>
2018-02-26 16:01:38 -0600

    updated api key

--- MovieHistory/MovieHistory/wwwroot/js/site.js
+++ MovieHistory/MovieHistory/wwwroot/js/site.js
@@ -2,7 +2,7 @@ $("#movieSearch__button").click(evt => {
     const userSearchString = $("#movieSearch").val()
     $.ajax({
         method: "GET",
-        url: `https://api.themoviedb.org/3/search/movie?api_key=858deec9a8305f575390bb92f4c3eab8f&language=en-US&query=${userSearchString}&page=1&include_adult=false`
+        url: `https://api.themoviedb.org/3/search/movie?api_key=858deec9a8305f575390bb92f4c3eab8&language=en-US&query=${userSearchString}&page=1&include_adult=false`
     }).then(res => {
         let titles = "<div class='row'>"
         res.results.forEach((m, idx) => {
