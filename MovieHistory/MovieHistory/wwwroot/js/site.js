2dd82d87096345a2cc505eac752c4d6d8348b5b3
Kristen Norris <krnorris65@gmail.com>
2018-02-27 14:48:46 -0600

    updated appsettings.json

--- MovieHistory/MovieHistory/wwwroot/js/site.js
+++ MovieHistory/MovieHistory/wwwroot/js/site.js
@@ -1,30 +1,4 @@
-<<<<<<< HEAD
 $("#movieSearch__button").click(evt => {
-    const userSearchString = $("#movieSearch").val()
-    $.ajax({
-        method: "GET",
-        url: `https://api.themoviedb.org/3/search/movie?api_key=858deec9a8305f575390bb92f4c3eab8&language=en-US&query=${userSearchString}&page=1&include_adult=false`
-    }).then(res => {
-        let titles = "<div class='row'>"
-        res.results.forEach((m, idx) => {
-            
-            titles += `
-                <div class="col-md-3 movieGrid__movie">
-                    <h2>${m.title}</h2>
-                    <img src="https://image.tmdb.org/t/p/w154${m.poster_path}" />
-                </div>
-            `
-            if ((idx + 1) % 4 === 0) {
-                titles += "</div><div class='row'>"
-            }
-        })
-
-        titles += "</div>"
-
-        $("#movieGrid").html(titles)
-    })
-=======
-﻿$("#movieSearch__button").click(evt => {
     const userSearchString = $("#movieSearch").val()
     $.ajax({
         method: "GET",
@@ -48,5 +22,4 @@ $("#movieSearch__button").click(evt => {
 
         $("#movieGrid").html(titles)
     })
->>>>>>> upstream/master
 });
